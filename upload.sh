a098ebc0e4638533877337032882902ddf9c0d8b
chrysn <chrysn@fsfe.org>
2020-11-28 12:28:48 +0100

    Fine-tuning of the upload process

--- upload.sh
+++ upload.sh
@@ -11,4 +11,8 @@ gio list ftps://rustdoc.etonomy.org/ >/dev/null
 
 # Given we can't set time stamps on this server, and checksumming would mean
 # getting the whole file, we trust that any changes are reflected in size changes
+#
+# Doing riot_wrappers first because those are the most active and provide
+# quickly checkable results as opposed to the possibly slow tail
+rsync -vr --ignore-times --perms ./target/i686-unknown-linux-gnu/doc/riot_wrappers /run/user/1000/gvfs/ftps:host=rustdoc.etonomy.org/
 rsync -vr --ignore-times --perms ./target/i686-unknown-linux-gnu/doc/riot_* /run/user/1000/gvfs/ftps:host=rustdoc.etonomy.org/
